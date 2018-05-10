5f3a89978500267f063cbd20235b87c044cf9648
Alexandra Hernandez <ms.hernandeza1@gmail.com>
2018-05-10 23:09:11 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,10 @@
 #write your code here
 
 def countdown(num)
-  until num < 0
-    "#{num} SECOND(S)!"
-    num -= 1
+  counter = num
+  until counter < 0
+    "#{counter} SECOND(S)!"
+    counter -= 1
   end
   "HAPPY NEW YEAR!"
 end
